07856fc56a4926e5c100961f1cd0b6a3477a6fe5
Mehebul02 <mehebul2123@gmail.com>
2024-02-24 11:44:55 +0600

    scope added

---
+++ scope.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    const total = a+b;
+    if(a > 5){
+        const sum = 4+a+b;
+    }
+    else{
+        const sum =10+a+b;
+        var current = sum;
+    }
+    console.log(current);
+    return total;
+}
+add(2,6);
