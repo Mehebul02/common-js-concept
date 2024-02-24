7d50050070b3a3ba512084f9914e6842eb0b6fe8
Mehebul02 <mehebul2123@gmail.com>
2024-02-24 11:31:25 +0600

    triple added

---
+++ triple.js
@@ -0,0 +1,12 @@
+// type coercion
+//type conversion
+
+const first = '0';
+const second = false;
+if(first == second){
+    console.log('Value is a equal');
+}
+else{
+    console.log('Value is not equal');
+}
+
